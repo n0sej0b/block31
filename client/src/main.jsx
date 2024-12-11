abbfca296b1d4c640089a733dac28294c77812f2
n0sej0b <garrett.l.rogers@gmail.com>
2024-12-10 21:40:50 -0700

    Updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,41 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import { createRoot } from 'react-dom/client'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import axios from 'axios';
 
-const App = () => {
-  const [notes, setNotes] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+const App = ()=> {
+  const [employees, setEmployees] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchNotes = async () => {
-      const response = await axios.get('/api/notes')
-      setNotes(response.data)
-      setIsLoading(false)
-    }
-    fetchNotes()
-  }, [])
+  useEffect(()=> {
+    const fetchEmployees = async()=> {
+      const response = await axios.get('/api/employees');
+      setEmployees(response.data);
+      setIsLoading(false);
+    };
+    fetchEmployees();
+  }, []);
 
-  if (isLoading) {
-    return <section className="loading">Loading</section>
+  if(isLoading){
+    return <section className='loading'>Loading</section>
   }
 
   return (
     <main>
-      <h1>Acme Notes ({notes.length})</h1>
+      <h1>Acme HR ({ employees.length })</h1>
       <ul>
-        {notes.map((note) => {
-          return (
-            <li key={note.id}>
-              {note.txt}
-              {note.starred ? '⭐' : null}
-            </li>
-          )
-        })}
+        {
+          employees.map( employee => {
+            return (
+              <li key={ employee.id }>
+                { employee.name }
+                {
+                  employee.is_admin ? <span style={{ paddingLeft: '1rem' }}>Administrator</span>: null
+                }
+              </li>
+            );
+          })
+        }
       </ul>
     </main>
-  )
-}
+  );
+};
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector('#root'));
 
-root.render(<App />)
+root.render(<App />);
--- server/index.js
+++ server/index.js
@@ -1,9 +1,44 @@
 // imports here for express and pg
+const express = require('express');
+const app = express();
+const path = require('path');
+const pg = require('pg');
 
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_hr_db')
 // static routes here (you only need these for deployment)
-
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../client/dist/index.html')))
+app.use(express.static(path.join(__dirname, '../client/dist')));
 // app routes here
-
+app.get('/api/employees', async (req, res, next) => {
+    try {
+        const SQL = `
+          SELECT * FROM employees;
+        `
+        const response = await client.query(SQL)
+        res.send(response.rows)
+    } catch (error) {
+        next(error)
+    }
+})
 // create your init function
-
+const init = async () => {
+    await client.connect()
+    const SQL = `
+      DROP TABLE IF EXISTS employees;
+      CREATE TABLE employees(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50),
+        is_admin BOOLEAN DEFAULT FALSE
+      );
+      INSERT INTO employees(name, is_admin) VALUES('Mickey Mouse', false);
+      INSERT INTO employees(name, is_admin) VALUES('Mini Mouse', false);
+      INSERT INTO employees(name, is_admin) VALUES('Goofy', true);
+      INSERT INTO employees(name, is_admin) VALUES('Daffy Duck', true);
+    `
+    await client.query(SQL)
+    console.log('data seeded')
+    const port = process.env.PORT || 3000
+    app.listen(port, () => `listening on port ${port}`)
+}
 // init function invocation
+init();
